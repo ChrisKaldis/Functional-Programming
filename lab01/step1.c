90e83233917527af7e6e7f8560ca590ec1551231
Christos Kaldis <up1059364@upnet.gr>
2025-09-08 17:23:07 +0300

    second exercise of step1

--- lab01/step1.c
+++ lab01/step1.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 
-void printfivechars(void);
+void printnchars(int n, char ch);
 
 int main() {
-    printfivechars();
+    printnchars(8, '=');
+    printnchars(22, '!');
 
     return 0;
 }
 
-void printfivechars(void) {
+/* Prints char ch n times. */
+void printnchars(int n, char ch) {
     int i;
 
-    for (i = 0; i < 5; ++i) {
-        printf("=");
+    for (i = 0; i < n; ++i) {
+        printf("%c", ch);
     }
     printf("\n");
 
